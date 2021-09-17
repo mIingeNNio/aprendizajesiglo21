5de283bd9ddc6d70e5e82aa2c434c62760803655
miingeNNio <89016695+mIingeNNio@users.noreply.github.com>
2021-09-17 10:05:11 -0500

    Agregar contenido a Maestros.vue

---
+++ src/views/Maestros.vue
@@ -0,0 +1,374 @@
+<template>
+  <div>
+ <div class="p-3 mb-2 bg-primary text-white"> 
+        <br>
+        <br>
+        <br><nav  class="navbar navbar-expand-lg navbar-dark bg-dark" >
+  <a class="navbar-brand" href="#">ADMINISTRADOR</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="acudiente.html">ACUDIENTE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="estudiante.html">ALUMNO</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          PROFESOR
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="profesor.html">INGLES</a>
+          <a class="dropdown-item" href="profesor.html">ESPAÑOL</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="profesor.html">MATEMATICAS</a>
+        </div>
+      </li>
+      
+    </ul>
+    
+  </div>
+</nav>
+ 
+
+<div class="p-3 mb-2 bg-primary text-white">
+
+  <br>
+  <br>
+  <br><!--<?php  require ("menu.php"); ?-->
+
+<div class="texto">
+  <br><B><div class="textotitulos">HORARIO DE CLASES PARA ESTUDIANTES Y PROFESORES</div></B>
+  <br>
+  <br>
+  <br>
+  <DIV class="container">
+    <div class="row">
+        <div class="col-md-5 col-md-push-5">
+        
+
+
+  <br>
+  <br>
+  <table class="table table-success table-striped">
+<thead class="table-dark">
+    "HORARIO DE CLASES ESTUDIANTES"
+<tr>
+<th scope="col">HORA</th>
+<th scope="col">DIA 1</th>
+<th scope="col">DIA 2</th>
+<th scope="col">DIA 3</th>
+<th scope="col">DIA 4</th>
+<th scope="col">DIA 5</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<th scope="row">11:50 - 12:45</th>
+<td>LENGUA CASTELLANA</td>
+<td><a href="matematicas.html">MATEOLOGÍA</a></td>
+<td>TECNOLOGÍA</td>
+<td>MATEMÁTICAS</td>
+<td>TECNOLOGÍA</td>
+</tr>
+<tr>
+<th scope="row">12:45 - 13:40</th>
+<td>INGLÉS</td>
+<td>INGLÉS</td>
+<td>C. SOCIALES</td>
+ <td>C. SOCIALES</td>
+  <td>RELIGIÓN</td>
+</tr>
+<tr>
+<th scope="row">13:40 - 14:35</th>
+<td>LENGUA CASTELLANA</td>
+<td>MATEOLOGÍA</td>
+<td>TECNOLOGÍA</td>
+<td>MATEOLOGÍA</td>
+<td>TECNOLOGÍA</td>
+</tr>
+<tr>
+<th scope="row">14:35 - 15:05</th>
+<td colspan="5"><pre><B>D            E           S           C          A          N            S        O</B></pre></td>
+</tr>
+<tr>
+<th scope="row">15:05 - 16:00</th>
+<td>LENGUA CASTELLANA</td>
+<td>MATEOLOGÍA</td>
+<td>TECNOLOGÍA</td>
+<td>MATEOLOGÍA</td>
+<td>TECNOLOGÍA</td>
+</tr>
+<tr>
+<th scope="row">16:00 - 16:55</th>
+<td>LENGUA CASTELLANA</td>
+<td>MATEOLOGÍA</td>
+<td>TECNOLOGÍA</td>
+<td>MATEOLOGÍA</td>
+<td>TECNOLOGÍA</td>
+</tr>
+<tr>
+<th scope="row">16:55 - 17:50</th>
+<td>LENGUA CASTELLANA</td>
+<td>MATEOLOGÍA</td>
+<td>TECNOLOGÍA</td>
+<td>MATEOLOGÍA</td>
+<td>TECNOLOGÍA</td>
+</tr>
+</tbody>
+</table>
+  <br></div>
+  <br>
+  <br><div class="col-md-2 col-md-push-2">
+    </div>
+  <br><div class="col-md-5 col-md-push-5">
+  
+  <br>
+  <br>
+<div class="table-responsive">
+<table class="table">
+<thead>
+   "HORARIO DE CLASES PROFESORES"
+<tr class="table-active">
+<th scope="col">HORA</th>
+<th scope="col">DIA 1</th>
+<th scope="col">DIA 2</th>
+<th scope="col">DIA 3</th>
+<th scope="col">DIA 4</th>
+<th scope="col">DIA 5</th>
+</tr>
+</thead>
+<tbody>
+<tr class="table-active">
+<th scope="row">11:50 - 12:45</th>
+<td>0801</td>
+<td>Proyecto Artes Escenicas</td>
+<td>Proyecto Emprendimiento</td>
+<td>Calificar</td>
+<td>s201</td>
+</tr>
+<tr class="table-active">
+<th scope="row">12:45 - 13:40</th>
+<td>s201</td>
+<td>Proyecto Artes</td>
+<td>1102</td>
+ <td>1101</td>
+  <td>1002</td>
+</tr>
+<tr class="table-active">
+<th scope="row">13:40 - 14:35</th>
+<td>1001</td>
+<td>0801</td>
+<td>Proyecto Robótica</td>
+<td>Proyecto Robótica</td>
+<td>s201</td>
+</tr>
+<tr>
+<th scope="row">14:35 - 15:05</th>
+<td colspan="5"><B><pre>D            E           S           C          A          N            S        O</pre></B></td>
+</tr>
+<tr class="table-active">
+<th scope="row">15:05 - 16:00</th>
+<td>1001</td>
+<td>1102</td>
+<td>1101</td>
+<td>Calificar</td>
+<td>1002</td>
+</tr>
+<tr class="table-active">
+<th scope="row">16:00 - 16:55</th>
+<td>S-201</td>
+<td>0901</td>
+<td>0902</td>
+<td>0902</td>
+<td>0901</td>
+</tr>
+<tr class="table-active">
+<th scope="row">16:55 - 17:50</th>
+<td>P. Robótica</td>
+<td>0901</td>
+<td>092</td>
+<td>0901</td>
+<td>0902</td>
+</tr>
+</tbody>
+</table>
+</div>
+  <br>
+  <br>
+  </div>
+</div>
+</DIV>
+
+</div>
+</div>
+</div>  
+
+        <footer class="page-footer dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h5>Empezar</h5>
+                    <ul>
+                        <li><a href="#">Hogar</a></li>
+                        <li><a href="#">Iniciar Sesión</a></li>
+                        <li><a href="#">Salir</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Institución</h5>
+                    <ul>
+                        <li><a href="#">Información Colegio</a></li>
+                        <li><a href="#">Contáctanos</a></li>
+                        <li><a href="#">Reglamento</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Apoyo</h5>
+                    <ul>
+                        <li><a href="#">Preguntas Frecuentes</a></li>
+                        <li><a href="#">Ayuda</a></li>
+                        <li><a href="#">Foro</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Legal</h5>
+                    <ul>
+                        <li><a href="#">Términos de Servicio</a></li>
+                        <li><a href="#">Términos de Uso</a></li>
+                        <li><a href="#">Políticas de Privacidad</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2021 MisiónTic UTP</p>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="https://cryptotelefono.com/wp-content/uploads/assets/js/smoothproducts.min.js"></script>
+    <script src="https://cryptotelefono.com/wp-content/uploads/assets/js/theme.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+   function s(){
+            var nombre1 = document.getElementById("nombre");
+                 document.getElementById("estudiantes").innerHTML = nombre1.value;
+                 var grado1 = document.getElementById("grado");
+                 document.getElementById("grados").innerHTML = grado1.value;
+                 var matriculam1 = document.getElementById("matriculam");
+                 document.getElementById("matriculasm").innerHTML = matriculam1.value;
+                 document.querySelector("mensaje").innerHTML = 'Â¡Hola mundo!';
+        }
+    
+        function suma(){
+           var primerV = document.getElementById('valor1').value;
+           var segundov= document.getElementById('valor2').value;
+            document.getElementById("primervalor").innerHTML = primerV;
+                   document.getElementById("segundovalor").innerHTML = segundov;
+           if(((parseInt(primerV) === 10000) || (parseInt(primerV) === -20000)) && ((parseInt(segundov) === -20000)|| (parseInt(segundov) === 10000))) {
+              return parseInt(primerV) + parseInt(segundov);
+               }else{
+                    return "Valor o valores incorrectos";
+               }
+             }
+    function imprimirSuma(){
+        
+            document.getElementById("resultadoSuma").innerHTML = suma();
+        }
+        function totalcompa(){
+            var comp1 = document.getElementById('compa1').value;
+           var comp2= document.getElementById('compa2').value;
+           var comp3 = document.getElementById('compa3').value;
+           var comp4= document.getElementById('compa4').value;
+           var totalcompan = parseInt(comp2)+parseInt(comp3)+parseInt(comp1)+parseInt(comp4);
+           var deuda= parseInt(comp2)+parseInt(comp3);
+           document.getElementById("dinerodeuda").innerHTML=deuda;
+           document.getElementById("dinerocompa").innerHTML=totalcompan;
+           //return parseInt(comp1)+parseInt(comp2)+parseInt(comp3)+parseInt(comp4);
+        }
+    function totales(){
+        document.getElementById("a").innerHTML=71;
+        document.getElementById("b").innerHTML=31;
+        document.getElementById("c").innerHTML=59;
+        document.getElementById("d").innerHTML=19;
+        document.getElementById("e").innerHTML=-19;
+        document.getElementById("f").innerHTML=-130;
+        document.getElementById("g").innerHTML=40;
+        document.getElementById("h").innerHTML=-161;
+        document.getElementById("i").innerHTML=-1585;
+    
+    }
+}
+</script>
+#secuencia{
+    width: 100%;
+    max-width:60%;
+     font-size: 18px;
+     text-align: left;
+     font-family: 'Chewy', cursive;
+     border-radius: 85px;
+     box-shadow: inset 15px 12px 12 px 5px rgba(108, 198, 226, 0.247), inset -4px -2px -8px, rgba(6, 86, 106, 0.959);
+     box-align:center;
+     padding: 5px;
+     margin-top: 5px;
+     margin-left: 5px;
+     border: 7px solid rgba(82, 3, 16, 0.877);
+     background: rgba(126, 196, 236, 0.802);
+}
+#hoja {
+    align-content:center;
+    width: 900px;
+    font:200px;
+    background-color:  rgba(28, 111, 236, 0.26);
+    font-family: 'Comic Sans serif','Courier New', Courier, monospace;
+    -moz-box-pack: rgba(35, 79, 200, 0.295);
+background: linear-gradient(to bottom, rgba(10, 54, 200, 0.705), rgba(157, 203, 233, 0.829));
+background-image: linear-gradient(to bottom, rgba(10, 52, 129, 0.507), #5a9ecb 60%, rgba(42, 133, 133, 0.527),0.5);
+-moz-box-shadow: 6px 8px 6px #999;
+box-shadow: 7px 5px 3px #060;
+-webkit-box-shadow: 5px 6px 4px rgba(105, 179, 240, 0.822);
+    margin-left: 25px;
+    margin-top: 50px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    padding-bottom: 20px;
+    padding-left: 15px;
+    padding-right: 25px;
+    padding-top: 20px;
+    border: 7px solid ;
+    border-color: blue;
+    border-radius : 50px;
+    border-bottom-left-radius: 25px;
+   
+    text-align: left;
+}
+.texto{
+    font-size: 10px;
+    padding: 5px;
+    margin: 10px;
+}
+.textotitulos{
+    font-size: 30px;
+    padding: 5px;
+    margin: 20px;
+    text-align: center;
+}
+<style>
+
+</style>
