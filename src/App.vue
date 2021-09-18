30568ffc42b1c6467c8aa29da5dfa73124b06260
miingeNNio <89016695+mIingeNNio@users.noreply.github.com>
2021-09-18 06:07:01 -0500

    Pruebas enlace router maestros

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/Maestro">Maestro</router-link>
+      <router-link to="/Maestros">Maestros</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -11,8 +11,8 @@ const routes = [
     component: Home
   },
   {
-    path: '/Maestro',
-    name: 'Maestro',
+    path: '/Maestros',
+    name: 'Maestros',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
